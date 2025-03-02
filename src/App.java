d8e57e53f609aca43bc381329ab36cf17a59803b
Kauan Martins <100448086+LeoneProgrammer@users.noreply.github.com>
2025-03-02 12:55:54 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int idade;
+        double altura;
+        String nome;
+
+        System.out.println("Digite seu nome e sobrenome: ");
+        nome = sc.nextLine();
+        System.out.println("Olá "+nome);
+        System.out.println("Poderia me informar sua idade e altura?");
+        idade = sc.nextInt();
+        altura = sc.nextDouble();
+        System.out.println("Muito bem "+nome+"você tem "+idade+" anos e "+altura+" metros de altura!");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int n1, n2;
+        int soma, sub, mult;
+        float div;
+
+        System.out.println("Digite o primeiro número: ");
+        n1 = scan.nextInt();
+        System.out.println("Digite o segundo número: ");
+        n2 = scan.nextInt();
+
+        soma = n1 + n2;
+        sub = n1 - n2;
+        mult = n1 * n2;
+        div = n1 / n2;
+
+        System.out.println("A soma dos núemros é: "+soma );
+        System.out.println("A subtração dos núemros é: "+sub );
+        System.out.println("A multiplicação dos núemros é: "+mult );
+        System.out.println("A divisão dos núemros é: "+div );
+
+    }
+
+}
