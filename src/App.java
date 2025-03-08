652c67046e27294c7b91a3667e99cf7148355357
Kauan Martins <100448086+LeoneProgrammer@users.noreply.github.com>
2025-03-08 00:15:22 -0300

    alguns exercícios feitos em sala

--- src/App.java
+++ src/App.java
@@ -2,20 +2,81 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
+       
         Scanner sc = new Scanner(System.in);
 
-        int idade;
-        double altura;
-        String nome;
+        /* Atv - 1 */
+        
+        // int idade;
+        // double altura;
+        // String nome;
 
-        System.out.println("Digite seu nome e sobrenome: ");
-        nome = sc.nextLine();
-        System.out.println("Olá "+nome);
-        System.out.println("Poderia me informar sua idade e altura?");
-        idade = sc.nextInt();
-        altura = sc.nextDouble();
-        System.out.println("Muito bem "+nome+"você tem "+idade+" anos e "+altura+"m de altura!");
+        // System.out.println("Digite seu nome e sobrenome: ");
+        // nome = sc.nextLine();
+        // System.out.println("Olá "+nome);
+        // System.out.println("Poderia me informar sua idade e altura?");
+        // idade = sc.nextInt();
+        // altura = sc.nextDouble();
+        // System.out.println("Muito bem "+nome+"você tem "+idade+" anos e "+altura+"m de altura!");
+        
+
+
+        /* Exercício 1 */
+
+        // int n1, n2, n3;
+        // float m1;
+
+        // System.out.println("Escreva o primeiro número: ");
+        // n1 = sc.nextInt();
+        // System.out.println("Escreva o segundo número: ");
+        // n2 = sc.nextInt();
+        // System.out.println("Escreva o terceiro número: ");
+        // n3 = sc.nextInt();
+
+        // m1 = (n1 + n2 + n3)/3;
+        
+        // //maior número 
+        // if (n1>= n2 && n1>=n3) {
+        //     System.out.println("O maior número é: "+n1);
+        // }
+        // else if (n2>= n1 && n2>= n3) {
+        //     System.out.println("O maior número é: "+n2);
+        // }
+        // else if (n3>= n1 && n3>= n2) {
+        //     System.out.println("O maior número é: "+n3);
+        // }
+
+
+        // //menor número 
+        // if (n1<= n2 && n1<= n3) {
+        //     System.out.println("O menor número é: "+n1);
+        // }
+        // else if (n2<= n1 && n2<= n3) {
+        //     System.out.println("O menor número é: "+n2);
+        // }
+        // else if (n3<= n1 && n3<= n2) {
+        //     System.out.println("O menor número é: "+n3);
+        // }
+        // System.out.println("A média dos números é: "+m1);
+        
+        
+
+        /* Exercício 2 */  
+
+        // double n1;
+
+        // System.out.println("Digite um número: ");
+        // n1 = sc.nextDouble();
+        // if (n1 >= 100) {
+        //     System.out.println("Seu número é maior que 100!");
+        // } else{
+        //     System.out.println("Seu número é menor que 100!");
+        // }
+        
+
+        /* Exercício 3 */
+
+        
 
     }
 }
