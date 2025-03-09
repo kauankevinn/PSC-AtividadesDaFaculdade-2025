0c696ed7e9bd39282ba106bed0f9a8fb6dd04f3b
Kauan Martins <100448086+LeoneProgrammer@users.noreply.github.com>
2025-03-08 23:01:04 -0300

    testando(modificações no teste)

--- src/App.java
+++ src/App.java
@@ -130,15 +130,20 @@ public class App {
         // l1 = sc.nextInt();
         // l2 = sc.nextInt();
         // l3 = sc.nextInt();
-        // if (l1 == l2 && l1 == l3 && l2 == l3) {
-        //     //lados iguais 
-        //     System.out.println("Seu triângulo é um equilatero!");
-        // }else if (l1 != l2 && l1 != l3 && l2 != l3 ) {
-        //     //lados diferentes  
-        //     System.out.println("Seu triângulo é escaleno!");
+        //  //Verifica se os valores formam um triângulo válido
+        // if (l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1) {
+        //     if (l1 == l2 && l2 == l3) {
+        //          //lados iguais 
+        //         System.out.println("Seu triângulo é um equilatero!");
+        //     }else if (l1 != l2 && l1 != l3 && l2 != l3 ) {
+        //          //lados diferentes  
+        //         System.out.println("Seu triângulo é escaleno!");
+        //     }else{
+        //          //Dois lados iguais 
+        //         System.out.println("Seu triângulo é isóceles");
+        //     }
         // }else{
-        //     //Dois lados iguais 
-        //     System.out.println("Seu triângulo é isóceles");
+        //     System.out.println("Os valores informados não formam um triângulo!");
         // }
 
         /* Exercício 8 */
--- src/Teste.java
+++ src/Teste.java
@@ -3,26 +3,35 @@ import java.util.Scanner;
 public class Teste {
     public static void main(String[] args) {
        
-        Scanner scan = new Scanner(System.in);
+        // Scanner scan = new Scanner(System.in);
         
-        int n1, n2;
-        int soma, sub, mult;
-        float div;
+        // int n1, n2;
+        // int soma, sub, mult;
+        // float div;
 
-        System.out.println("Digite o primeiro número: ");
-        n1 = scan.nextInt();
-        System.out.println("Digite o segundo número: ");
-        n2 = scan.nextInt();
+        // System.out.println("Digite o primeiro número: ");
+        // n1 = scan.nextInt();
+        // System.out.println("Digite o segundo número: ");
+        // n2 = scan.nextInt();
 
-        soma = n1 + n2;
-        sub = n1 - n2;
-        mult = n1 * n2;
-        div = n1 / n2;
+        // soma = n1 + n2;
+        // sub = n1 - n2;
+        // mult = n1 * n2;
+        // div = n1 / n2;
 
-        System.out.println("A soma dos núemros é: "+soma );
-        System.out.println("A subtração dos núemros é: "+sub );
-        System.out.println("A multiplicação dos núemros é: "+mult );
-        System.out.println("A divisão dos núemros é: "+div );
+        // System.out.println("A soma dos núemros é: "+soma );
+        // System.out.println("A subtração dos núemros é: "+sub );
+        // System.out.println("A multiplicação dos núemros é: "+mult );
+        // System.out.println("A divisão dos núemros é: "+div );
+
+
+        // //Testando arrays 
+        // int[] numeros = new int[3]; // Tamanho fixo de 3 elementos
+        // numeros[0] = 10;
+        // numeros[1] = 20;
+        // numeros[2] = 30;
+
+        // System.out.println("Primeiro número: " + numeros[1]); // Saída: 10
 
     }
 
